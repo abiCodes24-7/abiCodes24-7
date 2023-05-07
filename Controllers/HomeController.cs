2b635a6984f7086e4a46b0462b6b61b15da51e53
abiCodes24-7 <132831083+abiCodes24-7@users.noreply.github.com>
2023-05-07 20:41:20 +0530

    asp.mvc-CRUD operations

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using asp_mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace asp_mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+               
+            }
+            return View();
+
+
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,54 @@
+﻿
+using System;
+using System.ComponentModel.DataAnnotations;
+namespace asp_mvc.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter name")]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please choose gender")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Please enter data of birth")]
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
+        public DateTime DateofBirth { get; set; }
+
+        [Required(ErrorMessage = "Choose batch time")]
+        [Display(Name = "Batch time")]
+        [DataType(DataType.Time)]
+        public DateTime BatchTime { get; set; }
+
+
+        [Required(ErrorMessage = "please enter phone number")]
+        [Display(Name = "phone number")]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "please enter email addess")]
+        [Display(Name = "Email address")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "please enter website Url")]
+        [Display(Name = "Website Url")]
+        [Url]
+        public string website { get; set; }
+
+        [Required(ErrorMessage = "please enter password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "please confirm password")]
+        [Display(Name = "Consfirm password")]
+        [Compare("Password", ErrorMessage = "Password and confirm password does not match")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
